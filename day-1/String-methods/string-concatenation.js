6df62b4b7c477f4fa32435afc8b61473762d35fd
selvamani-gitaa <selvamani@gitaa.in>
2023-06-06 19:06:59 +0530

    day1

---
+++ day-1/String-methods/string-concatenation.js
@@ -0,0 +1,9 @@
+let x='Truth'
+let y=' is'
+let z=',I'
+let a=' am'
+let b=' Ironman'
+let c= 'Batman'
+console.log(x+y+z+a+b)
+let d='I am' ${c} 
+console.log(d)
---
+++ day-1/String-methods/trim.js
@@ -0,0 +1,5 @@
+//trim(): Removes trailing space in the beginning or the end of a string.
+let string = '   superman   '
+console.log(string)      
+console.log(string.trim(' '))  
+ 
