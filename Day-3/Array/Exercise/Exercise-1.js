84c110f01c91556f7ff895b25c9095cb981573a6
selvamani-gitaa <selvamani@gitaa.in>
2023-06-12 19:00:20 +0530

    Day-3

--- Day-3/Array/Exercise/Exercise-1.js
+++ Day-3/Array/Exercise/Exercise-1.js
@@ -35,16 +35,54 @@ console.log('Last Value - ',company[total-1])
 console.log('Middle Value - ',(company[Math.round(total/2)]))
 
 // Print out each company
-console.log(company[0],company[1],company[2],company[3],company[4],company[5],company[6])
+console.log(company.toString())
 // Change each company name to uppercase one by one and print them out
 let first=company[0].toString()
 console.log(first.toUpperCase())
+
 // Print the array like as a sentence: Facebook, Google, Microsoft, Apple, IBM,Oracle and Amazon are big IT companies.
-console.log(company[0,1,3,4,5])
+console.log(company.splice(5,0,'and'))
+console.log(company.toString(),"are big IT Companies")
 
+// Check if a certain company exists in the itCompanies array. If it exist return the company else return a company is not found
+let finish=company.includes('Amazon')
+console.log(finish)
+if(finish==true){
+    console.log("Include")
+}
+else{
+    console.log("Not Included")
+}
 
+// Filter out companies which have more than one 'o' without the filter method
+// 2
 
+// company[0].includes('o')
+// company[1].includes('o')
+// company[2].includes('o')
+// company[3].includes('o')
+// company[4].includes('o')
+// company[5].includes('o')
 
+// Sort the array using sort() method
+console.log(company.sort())
+// Reverse the array using reverse() method
+console.log(company.reverse())
+// Slice out the first 3 companies from the array
+console.log(company.slice(0,3))
+// Slice out the last 3 companies from the array
+console.log(company.slice(-3))
+// Slice out the middle IT company or companies from the array
+let v=company.slice(Math.round(company.length/2),(company.length/2)+1)
+console.log(v)
+// Remove the first IT company from the array
+console.log(company.shift())
+// Remove the middle IT company or companies from the array
+console.log(company.splice(Math.round(company.length/2),1))
+// Remove the last IT company from the array
+console.log(company.pop())
+// Remove all IT companies
+console.log(company.length=0)
 
 
 
