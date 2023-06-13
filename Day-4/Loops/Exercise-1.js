88e7678041b31d50fb3d4057efa74d5b4b7954d4
selvamani-gitaa <selvamani@gitaa.in>
2023-06-13 18:43:11 +0530

    Day-4

---
+++ Day-4/Loops/Exercise-1.js
@@ -0,0 +1,63 @@
+// Iterate 0 to 10 using for loop, do the same using while and do while loop
+for(i=0;i<=10;i++){
+    console.log(i)
+}
+
+i=0
+while(i<=10){
+
+    console.log(i)
+    i++
+}
+
+do{
+    console.log(i)
+    i++
+}while(i<=10){}
+
+// Iterate 10 to 0 using for loop, do the same using while and do while loop
+for(i=10;i>=0;i--){
+    console.log(i)
+}
+j=10
+while(j>=0){
+    console.log(j)
+    j--
+}
+k=10
+do{
+    console.log(k)
+    k--
+}while(k>=0)
+
+// Iterate 0 to n using for loop
+n=100
+for(i=0;i<=n;i++){
+    console.log(i)
+}
+
+// Write a loop that makes the following pattern using console.log():
+lines=6
+for(i=1;i<=lines;i++)
+{
+    line=''
+    for(j=0;j<i;j++){
+        line+="*"
+    }
+   console.log(line)
+}
+
+// Use loop to print the following pattern:
+
+for(i=0;i<=10;i++){
+    console.log(i,'x',i,'=',i*i)
+}
+
+// Using loop print the following pattern
+for(i=0;i<=10;i++){
+    console.log(i,'\t',i**2,'\t',i**3)
+}
+
+// Use for loop to iterate from 0 to 100 and print only even numbers
+for(i=0;)
+
---
+++ Day-4/Loops/test.js
@@ -0,0 +1,17 @@
+
+// // for( i=0;i<=20;i+=5){
+// //     console.log(i)
+// // }
+// i=100
+// while(i>=0){
+//     console.log(i)
+//     i-=5
+// }
+
+const new1=[]
+for(i=0;i<=500;i++){
+    if(i%3==0){
+        new1.push(i)
+    }
+}
+console.log(new1)
